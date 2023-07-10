c1bb30190d99a0e8b36dd5ff52cfcc0b72d7a212
John <john.cloghesy@gmail.com>
2023-07-10 04:46:06 -0600

    DDL: Setup Database: Update schema to create/implement - Users Table
     - Add SQL commands to the 01_schema.sql file to create Users Table
     - Include additional parameters necessary to fully implement table's schema 
     - Ensure to define column_name, data_type, column_constraint at a minimum

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,6 +1,13 @@
 -- ========================================================================= --
 -- ============================  01_SCHEMA.SQL  ============================ -- 
 
+-- Users Table                  --------------------------------------------
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
 
 
 
