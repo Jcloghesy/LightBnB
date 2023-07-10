65795859587a2b68c77f0d5bcc90f48a2b7346de
John <john.cloghesy@gmail.com>
2023-07-10 15:09:00 -0600

    SELECT: Create SQL Query - 04 Determining the most visited cities
     - Create sql query to select name of city and its number of reservations
     - Order the results from highest to lowest number of reservations

--- 1_queries/04_most_visited_cities.sql
+++ 1_queries/04_most_visited_cities.sql
@@ -1,6 +1,12 @@
 -- ========================================================================= --
 -- ===============  04_MOST_VISITED_CITIES.SQL  ============================ --
 
+SELECT properties.city, COUNT(reservations) AS total_reservations
+FROM reservations
+JOIN properties ON properties.id = property_id
+GROUP BY city
+ORDER BY total_reservations DESC;
+
 
 -- ========================================================================= --
 -- ========================================================================= --
