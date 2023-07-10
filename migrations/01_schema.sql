725b6631326e7e1445912e74548ee9bcb1a1f841
John <john.cloghesy@gmail.com>
2023-07-10 04:51:38 -0600

    DDL: Setup Database: Update schema to create/implement - Reservations Table
     - Add SQL commands to the 01_schema.sql file to create Reservations Table
     - Include additional parameters necessary to fully implement table's schema 
     - Ensure to define column_name, data_type, column_constraint at a minimum

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -32,6 +32,14 @@ CREATE TABLE properties (
   active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
+-- Reservations Table           --------------------------------------------
+CREATE TABLE reservations (
+  id SERIAL PRIMARY KEY NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
 
 -- ========================================================================= --
 -- ========================================================================= --
