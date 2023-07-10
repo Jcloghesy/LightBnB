e32d3b297f1dae46d98c8660f67e145f7eaca547
John <john.cloghesy@gmail.com>
2023-07-10 15:06:11 -0600

    SELECT: Create SQL Query - 03 Selecting property listings by city
     - Create sql query to select the id, title, cost_per_night, and an average_rating for Vancouver properties
     -  Order the results from lowest cost_per_night to highest cost_per_night.
     - Only show listings that have a rating >= 4 stars and limit the number of results to 10

--- 1_queries/03_property_listings_by_city.sql
+++ 1_queries/03_property_listings_by_city.sql
@@ -1,6 +1,15 @@
 -- ========================================================================= --
 -- ===============  03_PROPERTY_LISTINGS_BY_CITY.SQL  ====================== --
 
+SELECT properties.id, title, cost_per_night, avg(property_reviews.rating) as average_rating
+FROM properties
+LEFT JOIN property_reviews ON properties.id = property_id
+WHERE city LIKE '%ancouv%'
+GROUP BY properties.id
+HAVING avg(property_reviews.rating) >= 4
+ORDER BY cost_per_night
+LIMIT 10;
+
 
 -- ========================================================================= --
 -- ========================================================================= --
