4095da2f04ab685dabb1d662face624a436b8374
John <john.cloghesy@gmail.com>
2023-07-11 01:44:13 -0600

    PROPERTIES: Refactor Function - draw on db & enable filtering - getAllProperties
     - Refactor function to draw additional/all property information from lightbnb database
     - Update SELECT clause to return the necessary data to render the front end correctly
     - Use node-postgress query and to have function returns as promise 
    -  Ensure function can accept arguments that will enable filtering

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -58,7 +58,7 @@
       });
   };
 
-//  addUser             *--------------------------------------------------- 
+//  addUser             *-------------------------------------------------- 
   /**
    * Add a new user to the database
    * @param {{name: string, password: string, email: string}} user
@@ -105,14 +105,67 @@
   };
 
 //  =====================       PROPERTIES        =========================== 
-//  getAllProperties    UPDATED -------------------------------------------
+//  getAllProperties    *--------------------------------------------------
   /**
-   * Get all properties
-   * @param {{}} options An object containing query options
-   * @param {*} limit the number of results to return
-   * @return {Promise<[{}]>}  a promise to the properties
+   * Get all properties.
+   * @param {{}} options An object containing query options.
+   * @param {*} limit The number of results to return.
+   * @return {Promise<[{}]>}  A promise to the properties.
    */
-  const getAllProperties = (options, limit = 10) => {
+  const getAllProperties = function (options, limit = 10) {
+    const queryParams = [];
+    let queryString = `
+    SELECT properties.*, avg(property_reviews.rating) as average_rating
+    FROM properties
+    JOIN property_reviews ON properties.id = property_id
+    `;
+  
+    if (options.city) {
+      queryParams.push(`%${options.city}%`);
+      queryString += `WHERE city LIKE $${queryParams.length} `;
+    }
+  
+    if (options.owner_id) {
+      queryParams.push(options.owner_id);
+      queryString += `WHERE owner_id = $${queryParams.length} `;
+    }
+    
+    if (options.minimum_price_per_night && options.maximum_price_per_night) {
+      const minPriceInCents = options.minimum_price_per_night * 100;
+      const maxPriceInCents = options.maximum_price_per_night * 100;
+      queryParams.push(minPriceInCents, maxPriceInCents);
+      queryString += `${queryParams.length === 2 ? 'WHERE' : 'AND'} cost_per_night >= $${queryParams.length - 1} AND cost_per_night <= $${queryParams.length} `;
+    } else if (options.minimum_price_per_night) {
+      const minPriceInCents = options.minimum_price_per_night * 100;
+      queryParams.push(minPriceInCents);
+      queryString += `${queryParams.length === 1 ? 'WHERE' : 'AND'} cost_per_night >= $${queryParams.length} `;
+    } else if (options.maximum_price_per_night) {
+      const maxPriceInCents = options.maximum_price_per_night * 100;
+      queryParams.push(maxPriceInCents);
+      queryString += `${queryParams.length === 1 ? 'WHERE' : 'AND'} cost_per_night <= $${queryParams.length} `;
+    }
+  
+  
+    queryString += `
+    GROUP BY properties.id
+    `;
+
+    if (options.minimum_rating) {
+      queryParams.push(options.minimum_rating);
+      queryString += `HAVING AVG(property_reviews.rating) >= $${queryParams.length}`;
+    }
+
+    queryParams.push(limit);
+    queryString += `
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+    `;
+      
+    console.log(queryString, queryParams);
+    
+    return pool.query(queryString, queryParams).then((res) => res.rows);
+  };
+
     return pool
       .query(`SELECT * FROM properties LIMIT $1`, [limit])
       .then((result) => {
