565ae14c8f58d68d39e51823a13c4b190f007b79
John <john.cloghesy@gmail.com>
2023-07-11 00:12:25 -0600

    RESERVATIONS: Refactor Function - draws real res data - getAllReservations
     - Refactor function to draw real reservations from lightbnb database for a  specific user
     - Use node-postgress query and to have function returns as promise 
     - Ensure retrieves all necessary data needed to render the list of reservations for the user

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -79,15 +79,30 @@
 //  =====================       RESERVATIONS      ===========================
 //  getAllReservations  *-------------------------------------------------- 
   /**
-   * Get all reservations for a single user
-   * @param {string} guest_id The id of the user
-   * @return {Promise<[{}]>} A promise to the reservations
+   * Get all reservations for a single user.
+   * @param {string} guest_id The id of the user.
+   * @return {Promise<[{}]>} A promise to the reservations.
    */
   const getAllReservations = function (guest_id, limit = 10) {
-    return getAllProperties(null, 2);
-  }
-      };
-
+    return pool
+      .query(`
+      SELECT properties.*, reservations.start_date, AVG(property_reviews.rating) AS average_rating
+      FROM reservations
+      JOIN properties ON reservations.property_id = properties.id
+      JOIN property_reviews ON properties.id = property_reviews.property_id
+      WHERE reservations.guest_id = $1
+      GROUP BY properties.id, reservations.id
+      ORDER BY reservations.start_date
+      LIMIT $2;
+    `, [guest_id, limit])
+      .then((res) => {
+        console.log(res.rows);
+      return res.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+  };
 
 //  =====================       PROPERTIES        =========================== 
 //  getAllProperties    UPDATED -------------------------------------------
