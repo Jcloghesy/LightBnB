fa027855a7141d8b7b0ba186ae11ad43bd7ce55c
John <john.cloghesy@gmail.com>
2023-07-10 21:01:14 -0600

    WEB BOILERPLATE: Refactor function - draw from lightbnb - getAllProperties
     - Ensure function draws data from lightbnb database using node-postgress query
     - Return the appropriate value from our function as the result of a promise

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -79,20 +79,23 @@
 
 
 //  =====================       PROPERTIES        =========================== 
-//  getAllProperties    *--------------------------------------------------
+//  getAllProperties    UPDATED -------------------------------------------
   /**
    * Get all properties
    * @param {{}} options An object containing query options
-   * @param {*} limit The number of results to return
-   * @return {Promise<[{}]>}  A promise to the properties
+   * @param {*} limit the number of results to return
+   * @return {Promise<[{}]>}  a promise to the properties
    */
-  const getAllProperties = function (options, limit = 10) {
-    const limitedProperties = {};
-    for (let i = 1; i <= limit; i++) {
-      limitedProperties[i] = properties[i];
-    }
-    return Promise.resolve(limitedProperties);
-  };
+  const getAllProperties = (options, limit = 10) => {
+    return pool
+      .query(`SELECT * FROM properties LIMIT $1`, [limit])
+      .then((result) => {
+        console.log(result.rows);
+        return result.rows;
+      })
+      .catch((err) => {
+        console.log(err.message);
+  });
 
 //  addProperty         -------------------------------------------------- 
   /**
