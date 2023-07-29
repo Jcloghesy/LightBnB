0e3382034abd9132205cba3d9e0563f6d94ffc59
John <john.cloghesy@gmail.com>
2023-07-28 22:00:04 -0600

    Functional Requirements: Refactor Query - getAllReservations (required fields)
     - Ensure query selects all required columns including id,  title,  costpernight, startdate , averagerating 
     - Update Where cause to reference revervatoins.end_date
     - Correct the typo, by removing the errant backslash '\' character

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -87,11 +87,11 @@
   const getAllReservations = function (guest_id, limit = 10) {
     return pool
       .query(`
-      SELECT properties.*, reservations.start_date, AVG(property_reviews.rating) AS average_rating
+      SELECT properties.*, reservations.*, AVG(property_reviews.rating) AS average_rating
       FROM reservations
       JOIN properties ON reservations.property_id = properties.id
       JOIN property_reviews ON properties.id = property_reviews.property_id
-      WHERE reservations.guest_id = $1
+      WHERE reservations.guest_id = $1 AND reservations.end_date < now()
       GROUP BY properties.id, reservations.id
       ORDER BY reservations.start_date
       LIMIT $2;
