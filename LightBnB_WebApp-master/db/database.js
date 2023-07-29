d33d9c43674361c518247453a8a64b220ac9c3e4
John <john.cloghesy@gmail.com>
2023-07-28 22:38:06 -0600

    Functional Requirements: Refactor Query - getAllProperties ()
     - Updated the query to better handle multiple criteria ensuring no syntax error 
     - Replace WHERE with AND in conditional statements

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -114,59 +114,68 @@
    * @return {Promise<[{}]>}  A promise to the properties.
    */
   const getAllProperties = function (options, limit = 10) {
+
     const queryParams = [];
     let queryString = `
-    SELECT properties.*, avg(property_reviews.rating) as average_rating
-    FROM properties
-    JOIN property_reviews ON properties.id = property_id
-    `;
+      SELECT properties.*, AVG(property_reviews.rating) AS average_rating 
+      FROM properties 
+      JOIN property_reviews ON properties.id = property_id 
+      `;
   
     if (options.city) {
-      queryParams.push(`%${options.city}%`);
-      queryString += `WHERE city LIKE $${queryParams.length} `;
+    queryParams.push(`%${options.city.toLowerCase()}%`);
+    queryString += `WHERE LOWER(city) LIKE $${queryParams.length} `;
     }
   
     if (options.owner_id) {
-      queryParams.push(options.owner_id);
+      queryParams.push(`%${options.owner_id}%`); 
       queryString += `WHERE owner_id = $${queryParams.length} `;
     }
-    
-    if (options.minimum_price_per_night && options.maximum_price_per_night) {
-      const minPriceInCents = options.minimum_price_per_night * 100;
-      const maxPriceInCents = options.maximum_price_per_night * 100;
-      queryParams.push(minPriceInCents, maxPriceInCents);
-      queryString += `${queryParams.length === 2 ? 'WHERE' : 'AND'} cost_per_night >= $${queryParams.length - 1} AND cost_per_night <= $${queryParams.length} `;
-    } else if (options.minimum_price_per_night) {
-      const minPriceInCents = options.minimum_price_per_night * 100;
-      queryParams.push(minPriceInCents);
-      queryString += `${queryParams.length === 1 ? 'WHERE' : 'AND'} cost_per_night >= $${queryParams.length} `;
-    } else if (options.maximum_price_per_night) {
-      const maxPriceInCents = options.maximum_price_per_night * 100;
-      queryParams.push(maxPriceInCents);
-      queryString += `${queryParams.length === 1 ? 'WHERE' : 'AND'} cost_per_night <= $${queryParams.length} `;
+  
+    if (options.minimum_price_per_night) {
+      queryParams.push(options.minimum_price_per_night * 100);
+      if (queryParams.length === 1) {
+        queryString += `WHERE cost_per_night > $${queryParams.length} `;
+      } else {
+        queryString += `AND cost_per_night > $${queryParams.length} `;
+      }
     }
   
+    if (options.maximum_price_per_night) {
+      queryParams.push(options.maximum_price_per_night * 100);
+      if (queryParams.length === 1) {
+        queryString += `WHERE cost_per_night$ < $${queryParams.length} `;
+      } else {
+        queryString += `AND cost_per_night < $${queryParams.length} `;
+      }
+    }
   
-    queryString += `
-    GROUP BY properties.id
-    `;
-
+    queryString += `GROUP BY properties.id `;
+  
     if (options.minimum_rating) {
       queryParams.push(options.minimum_rating);
-      queryString += `HAVING AVG(property_reviews.rating) >= $${queryParams.length}`;
+      if (queryParams.length === 1) {
+        queryString += `
+      HAVING avg(property_reviews.rating) >= $${queryParams.length} `;
+      } else {
+        queryString += ` 
+      HAVING avg(property_reviews.rating) >= $${queryParams.length} `;
+      }
     }
-
+  
     queryParams.push(limit);
-    queryString += `
-    ORDER BY cost_per_night
-    LIMIT $${queryParams.length};
-    `;
-      
+    queryString += `ORDER BY cost_per_night LIMIT $${queryParams.length};`;
+  
     console.log(queryString, queryParams);
-    
-    return pool.query(queryString, queryParams).then((res) => res.rows);
+  
+    return pool
+      .query(queryString, queryParams)
+      .then((result) => {
+        return result.rows;
+      })
+      .catch((err) => err.message);
   };
-
+  
 //  addProperty         *--------------------------------------------------
 /**
  * Add a property to the database
